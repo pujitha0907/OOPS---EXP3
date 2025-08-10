a04fbb19472e029b4c46106f011d931e240d166f
pujitha0907 <chayammagari@karunya.edu.in>
2025-08-10 15:47:37 +0530

    Update output.java

--- output.java
+++ output.java
@@ -1,4 +1,4 @@
 # OOPS---EXP3
 
 CountLettersDigits
-  <img width="305" height="166" alt="Image" src="https://github.com/user-attachments/assets/fa573709-69bc-4daa-86c0-c933cdfbff55" />
+<img width="305" height="166" alt="Image" src="https://github.com/user-attachments/assets/fa573709-69bc-4daa-86c0-c933cdfbff55" />
